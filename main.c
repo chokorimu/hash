16ee722a214a0d1dfb041d1162387eb0a615c28f
chokorimu <rezondavar@gmail.com>
2026-03-07 06:09:57 +0700

    benerin dikit

--- main.c
+++ main.c
@@ -2,44 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ARR_SIZE 10
+
 struct hash_node {
     int key;
     char value[100];
     struct hash_node* next;
 };
 
-int hash(int key, char value[100]) {
+int hash(char* value) {
     int index = 0;
-    int i = 0;
-    while(value[i] != '\0') {
-        index = 2*index;
-        index += value[i]++;
-        i++;
+    for (int i = 0; value[i] != '\0'; i++) {
+        index += value[i];
     }
-    return index % 10;
+    return index % ARR_SIZE;
 }
 
-int insert(struct hash_node hash_table, int key, char value[100]) {
-    int index = hash(key, value);
+void insert(struct hash_node** hash_table, int key, char* value) {
+    int index = hash(value);
+
     struct hash_node* newNode = malloc(sizeof(*newNode));
     newNode->key = key;
-    newNode->value = value;
+    strcpy(newNode->value, value);
     newNode->next = NULL;
 
-    if(hash_table[index] != NULL) {
+    if(hash_table[index] == NULL) {
         hash_table[index] = newNode;
         return;
     }
-    struct hash_node* cursor = &(hash_table[index]);
+    struct hash_node* cursor = hash_table[index];
     while(cursor->next != NULL) {
         cursor=cursor->next;
     }
-    hash_table[index]->next = newNode;
+    cursor->next = newNode;
 }
 
 int main() {
-    struct hash_node hash_table[10];
-    insert(hash_node, 3, "hello");
+    struct hash_node* hash_table[ARR_SIZE] = {NULL};
+    insert(hash_table, 3, "hello");
 
     return 0;
 }
